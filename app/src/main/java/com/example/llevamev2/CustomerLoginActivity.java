2af41751410b16f41e5378a7b8112f23d5c2f402
joel <joelitoelcapo@gmail.com>
2020-05-04 22:58:33 -0400

    esta app esta siendo integrada a github

---
+++ app/src/main/java/com/example/llevamev2/CustomerLoginActivity.java
@@ -0,0 +1,110 @@
+package com.example.llevamev2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class CustomerLoginActivity extends AppCompatActivity {
+    private Button mLoginDriverButton,mSinginDrivererButton ;
+    private EditText mEmailCustomerEditText,mPasswordDriverEditText3 ;
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user!= null){
+                    Intent intent = new Intent(CustomerLoginActivity.this,DriverMapsActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            }
+
+        };
+
+
+        mEmailCustomerEditText = (EditText) findViewById(R.id.emailDrivererEditText);
+        mPasswordDriverEditText3 = (EditText) findViewById(R.id.passwordDriverEditText3);
+
+        mLoginDriverButton = (Button) findViewById(R.id.loginDriverButton);
+        mSinginDrivererButton = (Button) findViewById(R.id.singinDriverButton);
+
+        mSinginDrivererButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                final String email = mEmailCustomerEditText.getText().toString();
+                final String password = mPasswordDriverEditText3.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CustomerLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(CustomerLoginActivity.this, "singn up error", Toast.LENGTH_SHORT).show();
+                        }else{
+                            String user_id = mAuth.getCurrentUser().getUid();
+                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(user_id);
+                            current_user_db.setValue(true); //ojo en esta parte
+                        }
+                    }
+                });
+            }
+        });
+
+        mLoginDriverButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                final String email = mEmailCustomerEditText.getText().toString();
+                final String password = mPasswordDriverEditText3.getText().toString();
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(CustomerLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(CustomerLoginActivity.this, "longin in error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthListener);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/llevamev2/DriverMapsActivity.java
@@ -0,0 +1,46 @@
+package com.example.llevamev2;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
+public class DriverMapsActivity extends FragmentActivity implements OnMapReadyCallback  {
+
+    private GoogleMap mMap;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+}
